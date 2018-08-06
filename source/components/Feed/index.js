41d43974a5288b7a21e5790117acb0bb8499b1d9
Anton Khudiakov <ankhu@ciklum.com>
2018-08-06 21:31:13 +0300

    state homework

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -7,19 +7,30 @@ import StatusBar from '../StatusBar';
 
 // Instruments
 import Styles from './styles.m.css';
+import Spinner from '../Spinner';
 
 export default class Feed extends Component {
+    state = {
+        posts:      [{ id: '123', comment: 'Hi there!', created: 15365646546 }, { id: '456', comment: 'Приветик! ', created: 2646456 }],
+        isSpinning: true,
+    };
     render () {
         const { avatar, currentUserFirstName, currentUserLastName } = this.props;
+        const { posts } = this.state;
+
+        const postsJSX = posts.map((post) => {
+            return <Post key = { post.id } { ...post } />;
+        });
 
         return (
             <section className = { Styles.feed }>
+                <Spinner isSpinning = { this.state.isSpinning } />
                 <StatusBar { ...this.props } />
                 <Composer
                     avatar = { avatar }
                     currentUserFirstName = { currentUserFirstName }
                 />
-                <Post { ...this.props } />
+                {postsJSX}
             </section>
         );
     }
